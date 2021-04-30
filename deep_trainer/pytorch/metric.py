dc30ad6a42a6c2fef1a71135e8dbced08450520e
Raphael Reme <raphaelreme-dev@protonmail.com>
2021-04-30 10:38:36 +0200

    metric: Renamed label in target for consistency

--- deep_trainer/pytorch/metric.py
+++ deep_trainer/pytorch/metric.py
@@ -11,20 +11,20 @@ class Criterion:
     def __init__(self, name: str):
         self.name = name
 
-    def __call__(self, predictions: torch.Tensor, labels: torch.Tensor) -> torch.Tensor:
+    def __call__(self, predictions: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
         """Update the criterion
 
         See `Criterion.update`
         """
-        return self.update(predictions, labels)
+        return self.update(predictions, targets)
 
-    def update(self, predictions: torch.Tensor, labels: torch.Tensor) -> torch.Tensor:
+    def update(self, predictions: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
         """Update the criterion with the current batch, and returns the loss of this batch
         if this make sense.
 
         Args:
             predictions (torch.Tensor): Output of the model
-            labels (torch.Tensor): Target values
+            targets (torch.Tensor): Target values
 
         Returns:
             torch.Tensor: The loss of the current batch if it exists. ("nan" otherwise)
@@ -60,7 +60,7 @@ class AveragingCriterion(Criterion):
 
         Args:
             loss_function (callable): Expect a pytorch function that should compute a loss given
-                predictions and labels
+                predictions and targets
         """
         if name is None:
             name = getattr(loss_function, "__name__", getattr(loss_function, "__class__").__name__)
@@ -69,9 +69,9 @@ class AveragingCriterion(Criterion):
         self._sum = 0
         self._n_samples = 0
 
-    def update(self, predictions: torch.Tensor, labels: torch.Tensor) -> torch.Tensor:
-        loss = self.loss_function(predictions, labels)
-        batch_size = labels.shape[0]
+    def update(self, predictions: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
+        loss = self.loss_function(predictions, targets)
+        batch_size = targets.shape[0]
         self._sum += loss.item() * batch_size
         self._n_samples += batch_size
         return loss
@@ -79,7 +79,7 @@ class AveragingCriterion(Criterion):
     def aggregate(self) -> float:
         return self._sum / self._n_samples
 
-    def reset(self):
+    def reset(self) -> "AveragingCriterion":
         self._sum = 0
         self._n_samples = 0
         return self
@@ -88,65 +88,65 @@ class AveragingCriterion(Criterion):
 class Accuracy(AveragingCriterion):
     """Accuracy criterion.
 
-    Compute the proportion of corrected predicted labels. (Multiclass classification)
+    Compute the proportion of corrected predicted targets. (Multiclass classification)
     """
 
     def __init__(self):
         super().__init__(self._compute, "Accuracy")
 
     @staticmethod
-    def _compute(predictions: torch.Tensor, labels: torch.Tensor) -> torch.Tensor:
-        predicted_labels = torch.argmax(predictions, dim=1)
-        return torch.sum(predicted_labels == labels) / torch.numel(labels)
+    def _compute(predictions: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
+        predicted_targets = torch.argmax(predictions, dim=1)
+        return torch.sum(predicted_targets == targets) / torch.numel(targets)
 
 
 class Error(AveragingCriterion):
     """Error criterion.
 
-    Compute the proportion of wrongly predicted labels. (Multiclass classification)
+    Compute the proportion of wrongly predicted targets. (Multiclass classification)
     """
 
     def __init__(self):
         super().__init__(self._compute, "Error")
 
     @staticmethod
-    def _compute(predictions: torch.Tensor, labels: torch.Tensor) -> torch.Tensor:
-        predicted_labels = torch.argmax(predictions, dim=1)
-        return torch.sum(predicted_labels != labels) / torch.numel(labels)
+    def _compute(predictions: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
+        predicted_targets = torch.argmax(predictions, dim=1)
+        return torch.sum(predicted_targets != targets) / torch.numel(targets)
 
 
 class BalancedAccuracy(Criterion):
     """Balanced Accuracy Criterion
 
-    Compute the Balanced Accuracy of the predictions. <=> Average the recall of each labels.
+    Compute the Balanced Accuracy of the predictions. <=> Average the recall of each target.
     This criterion has meaning only at aggregation time.
     """
 
     def __init__(self):
         super().__init__("BalancedAccuracy")
-        self.label_occurences = {}
+        self.target_occurences = {}
         self.true_positives = {}
 
-    def update(self, predictions: torch.Tensor, labels: torch.Tensor) -> torch.Tensor:
-        predicted_labels = torch.argmax(predictions, dim=1)
+    def update(self, predictions: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
+        predicted_targets = torch.argmax(predictions, dim=1)
 
-        assert predicted_labels.shape == labels.shape
-        assert len(predicted_labels.shape) == 1
+        assert predicted_targets.shape == targets.shape
+        assert len(predicted_targets.shape) == 1
 
-        for i in range(predicted_labels.shape[0]):
-            label = labels[i].item()
-            predicted = predicted_labels[i].item()
+        for i in range(predicted_targets.shape[0]):
+            target = targets[i].item()
+            predicted = predicted_targets[i].item()
 
-            self.label_occurences[label] = self.label_occurences.get(label, 0) + 1
-            self.true_positives[label] = self.true_positives.get(label, 0) + (label == predicted)
+            self.target_occurences[target] = self.target_occurences.get(target, 0) + 1
+            self.true_positives[target] = self.true_positives.get(target, 0) + (target == predicted)
 
         return torch.tensor(float("nan"))  # pylint: disable=not-callable
 
     def aggregate(self) -> float:
-        recall = [self.true_positives[key] / self.label_occurences[key] for key in self.label_occurences]
+        recall = [self.true_positives[target] / self.target_occurences[target] for target in self.target_occurences]
         return sum(recall) / len(recall)  # Averaged
 
-    def reset(self):
-        self.label_occurences = {}
+    def reset(self) -> "BalancedAccuracy":
+        self.target_occurences = {}
         self.true_positives = {}
         return self
